f5a49c6e73c078fac6f69c02389c412abd505234
Christopher Smith <csmith4work@gmail.com>
2019-06-04 01:33:59 -0500

    Setting up data persistence.

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const newUploadService = require('./services/NewUploadService').default;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', async (req, res) => {
-    res.send("Hello from the other side");
+app.get('/health', async (req, res) => {
+    res.send("Health check: Main API active.");
 });
 
 app.post('/', async (req, res) => {
     console.log(req.body);
-    res.send( req.body);
+    res.send(req.body);
 });
-//app.use('/api/tracks', tracks);
 
-app.listen(process.env.PORT || 80);
-console.log("Api running on port " + process.env.PORT);
+app.post('/upload', async (req, res) => {
+    console.log(req.body);
+    res.send(await newUploadService.newVideoUpload());
+});
+
+app.listen(process.env.PORT || 5000);
+console.log("Api running");

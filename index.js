bace6a57d532f99fdca17c1c76f78ecfb970e2fa
Christopher Smith <csmith4work@gmail.com>
2019-06-16 11:57:59 -0500

    extracted the key from event object and inserted it to a seperate db

--- index.js
+++ index.js
@@ -24,9 +24,13 @@ app.post('/upload', async (req, res) => {
 
 const newVideoUpload = async (data) => {
     const videos = await loadNewVideoEvents();
+    const keys = await loadNewVideoKeys();
     await videos.insertOne({
         name: JSON.stringify(data),
-      });
+    });
+    await keys.insertOne({
+        key: data.Records[0].s3.object.key,
+    })
     return "Check it!";
 };
 
@@ -38,5 +42,13 @@ const loadNewVideoEvents = async () => {
     return client.db('nurenqa1').collection('newvideoevents');
 };
 
+const loadNewVideoKeys = async () => {
+    const client = await mongodb.MongoClient.connect
+    ('mongodb://ObsidianTech:Obsidian12!@ds131737.mlab.com:31737/nurenqa1', {
+        useNewUrlParser: true
+    });
+    return client.db('nurenqa1').collection('nurenvideokeys');
+};
+
 app.listen(process.env.PORT || 80);
-console.log("Api running");
+console.log("Video Upload Api running");
